865594f7a7882f3e9fa940790fb625c99d893130
CIITSHSDTP00\CIIT <casanovawarleyne@gmail.com>
2024-10-22 12:57:24 +0800

    Int Changes
    Updating String Values to Int

--- Assets/ImpGUI.cs
+++ Assets/ImpGUI.cs
@@ -7,23 +7,44 @@ public class ImpGUI : MonoBehaviour
 {
 
     public string playerStats = "Player Stats";
+
+    int playerOneAttack = 100;
+    int playerOneDefense = 50;
+    int playerOneSpeed = 60;
+    int playerOneLuck = 10;
+    int playerOneIntelligence = 95;
+    int playerOneSpecial = 20;
+
+    int playerTwoAttack = 250;
+    int playerTwoDefense = 250;
+    int playerTwoSpeed = 30;
+    int playerTwoLuck = 50;
+    int playerTwoIntelligence = 60;
+    int playerTwoSpecial = 70;
+
+    int playerThreeAttack = 250;
+    int playerThreeDefense = 250;
+    int playerThreeSpeed = 30;
+    int playerThreeLuck = 50;
+    int playerThreeIntelligence = 60;
+    int playerThreeSpecial = 70;
     private void OnGUI()
     {
         GUI.Box(new Rect(50, 50, 1370, 620), "Player List");
 
         if (GUI.Button(new Rect(70, 100, 400, 100), "Player One"))
         {
-            playerStats = "Player One\r\nName: Christian Bum\r\nAttack: 100\r\nDefense: 50\r\nSpeed: 60\r\nLuck: 10\r\nIntelligence: 95\r\nSpecial: 20";
+            playerStats = "Player One\r\nName: Christian Bum" + "\r\nAttack: " + playerOneAttack + "\r\nDefense: " + playerOneDefense + "\r\nSpeed: " + playerOneSpeed + "\r\nLuck: " + playerOneLuck + "\r\nIntelligence: " + playerOneIntelligence + "\r\nSpecial: " + playerOneSpecial;
         }
 
         if (GUI.Button(new Rect(70, 300, 400, 100), "Player Two"))
         {
-            playerStats = "Player Two\r\nName: Sam Sulking\r\nAttack: 250\r\nDefense: 250\r\nSpeed: 30\r\nLuck: 50\r\nIntelligence: 60\r\nSpecial: 70";
+            playerStats = "Player Two\r\nName: Sam Sulking" + "\r\nAttack: " + playerTwoAttack + "\r\nDefense: " + playerTwoDefense + "\r\nSpeed: " + playerTwoSpeed + "\r\nLuck: " + playerTwoLuck + "\r\nIntelligence: " + playerTwoIntelligence + "\r\nSpecial: " + playerTwoSpecial;
         }
 
         if (GUI.Button(new Rect(70, 500, 400, 100), "Player Three"))
         {
-            playerStats = "Player Three\r\nName: Greg Dookie\r\nAttack: 175\r\nDefense: 200\r\nSpeed: 90\r\nLuck: 100\r\nIntelligence: 100\r\nSpecial: 50";
+            playerStats = "Player Three\r\nName: Greg Dookie" + "\r\nAttack: " + playerThreeAttack + "\r\nDefense: " + playerThreeDefense + "\r\nSpeed: " + playerThreeSpeed + "\r\nLuck: " + playerThreeLuck + "\r\nIntelligence: " + playerThreeIntelligence + "\r\nSpecial: " + playerThreeSpecial;
         }
 
         GUI.Label(new Rect(1000, 250, 150, 200), playerStats);
